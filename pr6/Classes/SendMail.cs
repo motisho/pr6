85378a7278b12d6a05367844ea163c31a10a1f55
bobvad <vladimirdegtannikov408@gmail.com>
2025-12-03 15:10:09 +0500

    Разработанный класс User. Юсупов Мамаев

---
+++ pr6/Classes/SendMail.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Net;
+using System.Net.Mail;
+
+namespace pr6.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string Message, string To)
+        {
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("yandex@yandex.ru", "password"),
+                EnableSsl = true,
+            };
+            smtpClient.Send("landaxer@yandex.ru", To, "Проект RegIn", Message);
+        }
+    }
+}
--- pr6/Classes/WorkingDB.cs
+++ pr6/Classes/WorkingDB.cs
@@ -6,15 +6,10 @@ namespace pr6.Classes
 {
     public class WorkingDB
     {
-    /// <summary>
-    /// Строка подключения к базе данных, указывается сервер, порт подключения, база данных, имя пользователя, пароль пользователя
-    /// </summary>
+   
     readonly static string connection = "server=localhost;port=3306;database=regin;user=root;pwd=root;";
 
-    /// <summary>
-    /// Создание и открытие подключения
-    /// </summary>
-    /// <returns>Открытое подключение или null</returns>
+   
     public static MySqlConnection OpenConnection()
     {
         try
@@ -30,33 +25,20 @@ namespace pr6.Classes
         }
     }
 
-    /// <summary>
-    /// Функция выполнения SQL-запросов
-    /// </summary>
-    /// <param name="Sql">SQL-запрос</param>
-    /// <param name="mySqlConnection">Открытое подключение</param>
-    /// <returns></returns>
     public static MySqlDataReader Query(string Sql, MySqlConnection mySqlConnection)
     {
         MySqlCommand mySqlCommand = new MySqlCommand(Sql, mySqlConnection);
         return mySqlCommand.ExecuteReader();
     }
 
-    /// <summary>
-    /// Функция закрытия соединения с базой данных
-    /// </summary>
-    /// <param name="mySqlConnection">Открытое MySQL соединение</param>
+
     public static void CloseConnection(MySqlConnection mySqlConnection)
     {
         mySqlConnection.Close();
         MySqlConnection.ClearPool(mySqlConnection);
     }
 
-    /// <summary>
-    /// Функция проверки соединение на работоспособность
-    /// </summary>
-    /// <param name="mySqlConnection">MySQL соединение</param>
-    /// <returns>Статус работоспособности соединения</returns>
+
     public static bool OpenConnection(MySqlConnection mySqlConnection)
     {
         return mySqlConnection != null && mySqlConnection.State == System.Data.ConnectionState.Open;
