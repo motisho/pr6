19641e8152ab8b8df342802693c4f3166e35aa40
bobvad <vladimirdegtannikov408@gmail.com>
2025-12-03 15:29:10 +0500

    Программирование окна MainWindow. Юсупов Мамаев

--- pr6/MainWindow.xaml.cs
+++ pr6/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using pr6.Classes;
 
 namespace pr6
 {
@@ -20,9 +11,35 @@ namespace pr6
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public static MainWindow mainWindow;
+
+        public User UserLogIn = new User();
         public MainWindow()
         {
             InitializeComponent();
+            OpenPage(new Pages.Login());
+        }
+        /// <summary>
+        /// Функция открытия страницы
+        /// </summary>
+        /// <param name="page">Страница которую необходимо открыть</param>
+        public void OpenPage(Page page)
+        {
+            DoubleAnimation StartAnimation = new DoubleAnimation();
+            StartAnimation.From = 1;
+            StartAnimation.To = 0;
+            StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            StartAnimation.Completed += delegate
+            {
+                frame.Navigate(page);
+                DoubleAnimation EndAnimation = new DoubleAnimation();
+                EndAnimation.From = 0;
+                EndAnimation.To = 1;
+                EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                frame.BeginAnimation(Frame.OpacityProperty, EndAnimation);
+            };
+            frame.BeginAnimation(Frame.OpacityProperty, StartAnimation);
         }
     }
 }
